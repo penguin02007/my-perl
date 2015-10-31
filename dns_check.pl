42b36bcc138c9d76eb88678b73e56990c8fb2b66
Leo Chan <penguin02007@gmail.com>
2015-10-30 20:50:37 -0400

    Removed split per Sing suggestion

--- dns_check.pl
+++ dns_check.pl
@@ -3,14 +3,12 @@ use feature qw(say);
 #  POSIX for non-printable characters
 # my $rex1 = qr/[^[:print:]]/;
 my $rex2 = qr/[^a-zA-Z\d\s\n\-_.]/;
-my $rex3 = qr/[^\w\-_.]+/;
+#my $rex3 = qr/[^\w\-_.]+/;
 my $matched = 0;
-my $file1 = 'replaced.txt';
 
 while (my $line = <>) {
 	chomp $line;
 #	@fields =  split /\s/, $line;
-#	die "$line is invalid\n" if $line !~ /^$rex3\s$rex3\s$rex3$/;
 	$line =~ s/$rex2//g && say $line && $matched++;
 	say $line;
 #	foreach my $field (@fields) {
