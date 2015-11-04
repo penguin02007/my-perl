bd82309c586da09b13e50e34cc52c7d13467c514
Leo Chan <penguin02007@gmail.com>
2015-11-04 02:05:46 -0500

    working

--- dns_check.pl
+++ dns_check.pl
@@ -1,20 +1,16 @@
 use feature qw(say);
 
-#  POSIX for non-printable characters
-# my $rex1 = qr/[^[:print:]]/;
-my $rex2 = qr/[^a-zA-Z\d\s\n\-_.]/;
-#my $rex3 = qr/[^\w\-_.]+/;
+my $rex3 = qr/[^\w\s\-_.]+/;
 my $matched = 0;
+my $file1 = '/tmp/out.txt';
+$^|=".bak";
+$\ = $/;
 
-while (my $line = <>) {
+while ( my $line = <> ) {
 	chomp $line;
-#	@fields =  split /\s/, $line;
-	$line =~ s/$rex2//g && say $line && $matched++;
-	say $line;
-#	foreach my $field (@fields) {
-#	$field =~ s/$rex1//g && $matched++;
-#    }
-#	open my $fh1, '>>:encoding(UTF-8)', $file1 or die "Could not open $file1";
+	$line =~ s/$rex3//g && $matched++;
+	open my $fh1, '>>:encoding(UTF-8)', $file1 or die "Could not open $file1";
+	say $fh1 $line;
 	close $fh1;	
 }
 say "$matched fields replaced";
