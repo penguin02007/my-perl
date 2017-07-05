438324fa0e0e9a2a1963bcff39ed4dea92ec7249
lchan <lchan@pg007.86c.nets.ws>
2017-07-05 00:11:03 -0400

    add/update comments

--- debug_proc.pl
+++ debug_proc.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# Capture process with ps
+# Capture process with ps and write to file
 #
 use v5.10.0;
 use warnings;
@@ -8,27 +8,26 @@ use strict;
 use DateTime;
 
 my $proc_result = get_proc();
-create_log(get_proc());
+create_log($proc_result);
 
 sub create_log {
   my $fh;
   my $sum = 0;
   my $result = shift(@_); #  subroutine argument
-  my $today = DateTime->now->strftime('%m%d%Y');
-  my $diag_file = "/tmp/diag_$today.txt";
-  open ($fh, '+>', $diag_file) #  open filehandle for read-write 
-    || die "Can't open $diag_file $!";
+  my $today = DateTime->now->strftime('%Y%m%d%H%M%S'); #  current time in UTC
+  my $debug_file = "/tmp/debug_$today.txt";
+  open ($fh, '+>', $debug_file) #  open for read-write
+    || die "Can't open $debug_file $!";
   print $fh $result;
-  open ($fh, '<', $diag_file) #  reopen file handle to count lines
-    || die "Can't open $diag_file $!";
+  open ($fh, '<', $debug_file) #  reopen to count lines
+    || die "Can't open $debug_file $!";
   while (<$fh>) {
     $sum++;
   }
-  say "Written $sum line in $diag_file";
+  say "Written $sum line in $debug_file";
   close $fh;
 }
 
 sub get_proc {
   my $cmd = `ps aux`;
-# return $cmd;
 }
