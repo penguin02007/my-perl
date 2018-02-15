b184e9bcf1e9811bc688b80eafcc8f4d776f9f8f
lchan <ka.chan@rakops.com>
2018-02-15 11:22:04 -0500

    print mac for all idrac

--- get-idrac-macs.pl
+++ get-idrac-macs.pl
@@ -1,16 +1,21 @@
 #! /usr/bin/env perl
 use v5.014;
 use Net::SSH::Perl;
+use Data::Dumper;
 
-for my $ip ( 137..138 ) {
-    my $ssh = Net::SSH::Perl->new("172.20.230.137");
-    $ssh->login("root", "calvin");
-    my $idrac_cmd = "racadm racdump";
-    my($stdout, $stderr, $exit) = $ssh->cmd("$idrac_cmd");
-    while ($stdout =~ m/MAC|Ethernet/) {
-        @macs = pop $_;
+my @matches,;
+my $idrac_cmd = "racadm racdump";
+foreach my $ip (137..168) {
+    my $ssh = Net::SSH::Perl->new( "172.20.230.$ip" );
+    say "looping 172.20.230.$ip";
+    push @matches, "172.20.230.$ip";
+    $ssh->login( "root", "calvin" ) ;
+    my ( $stdout, $stderr, $exit ) = $ssh->cmd( "$idrac_cmd" );
+    my @text = split '\n+', $stdout;
+    foreach (@text) {
+        push @matches, grep( /^MAC.+?$|(Ethernet.+?$)/, $_);
     }
 }
-while ( @macs ) {
-    say;
+foreach (@matches) {
+    say $1 if $_ =~ m/((?:\w\w:){5}\w\w)/;
 }
